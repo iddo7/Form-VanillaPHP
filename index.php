e1561f84de0bd357d3dbed1a4bc4ae460b2ef815
iddo <61661956+iddo7@users.noreply.github.com>
2023-08-28 12:54:48 -0400

    Ajout variables de value

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
-    <title>Index</title>
+    <title>Porjet 2</title>
 </head>
 <body>
     <div class="container">
@@ -15,7 +15,8 @@
                 <h1 class="mb-4">Porjet 2</h1>
 
                 <?php 
-                    $nom = $prenom = $avatar = "";
+                    $username = $password = $passwordXtra = $email = $gender = $birthday = $transport = "";
+                    $errorOccured = false;
 
 
                     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -37,31 +38,31 @@
                             ?>
                         " method="post">
                             <div class="input-group">
-                                <input type="text" name="username" id="username" placeholder="Username" class="form-control mb-3">
+                                <input type="text" name="username" id="username" placeholder="Username" class="form-control mb-3" value="<?php echo $nom;?>">
                             </div>
                             <div class="input-group">
-                                <input type="password" id="password" name="password" placeholder="Mot de passe" class="form-control mb-3">
-                                <input type="password" id="passwordXtra" name="passwordXtra" placeholder="Vérifier mot de passe" class="form-control mb-3">
+                                <input type="password" id="password" name="password" placeholder="Mot de passe" class="form-control mb-3" value="<?php echo $nom;?>">
+                                <input type="password" id="passwordXtra" name="passwordXtra" placeholder="Vérifier mot de passe" class="form-control mb-3" value="<?php echo $nom;?>">
                             </div>
                             <div class="input-group">
-                                <input type="email" id="email" name="email" placeholder="Adresse courriel" class="form-control mb-3">
+                                <input type="email" id="email" name="email" placeholder="Adresse courriel" class="form-control mb-3" value="<?php echo $nom;?>">
                             </div>
                             <div class="input-group">
                                 <a href="https://i.pinimg.com/originals/54/91/69/549169ad62f98b3731dd61558a26e6db.jpg">
                                     <button class="btn btn-dark">Avatar</button>
                                 </a>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="optionGender" id="optionMale">
+                                    <input class="form-check-input" type="radio" name="optionGender" id="optionMale" value="male" <?php echo $gender == 'male' ? 'checked' : '' ?>>
                                     <label class="form-check-label" for="optionMale">Masculin</label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="optionGender" id="optionFemale">
+                                    <input class="form-check-input" type="radio" name="optionGender" id="optionFemale" value="female" <?php echo $gender == 'female' ? 'checked' : '' ?>>
                                     <label class="form-check-label" for="optionFemale">Féminin</label>
                                 </div>
                             </div>
                             <div class="input-group">
                                 <label class="input-group-text" for="birthday">Date de naissance</label>
-                                <input type="date" id="birthday" name="birthday" class="form-control mb-3">
+                                <input type="date" id="birthday" name="birthday" class="form-control mb-3" value="<?php echo $nom;?>">
                             </div>
                             <div class="input-group">
                                 <label class="input-group-text" for="transport">Moyen de transport</label>
